22b5ef49a7caf5568d91193f0409527aefb68955
Thomas E Lackey <telackey@bozemanpass.com>
2024-05-22 23:33:05 -0500

    Add simple build script

---
+++ nitro-build.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+cd ../ts-nitro && yarn && yarn build:browser
+cd ../metamask-core && yarn && yarn build
+cd ../eth-json-rpc-nitro && yarn && yarn build
+cd ../metamask-extension
+
+if [ ! -f ".metamaskrc" ]; then
+  cp ".metamaskrc.dist" .metamaskrc
+fi
+
+yarn && yarn start --build-type flask 
